8205ac927ec75311aeb57781fa28f436e4fdba5a
“Krista <stadler.krista@gmail..com>
2020-10-02 16:56:33 -0600

    Adding logic to ticketing app

--- MasterTicket/master_ticket.py
+++ MasterTicket/master_ticket.py
@@ -1,10 +1,25 @@
 TICKET_PRICE = 10
 tickets_remaining = 100
 
-print("Tickets Remaining: {}".format(tickets_remaining))
-name = input("What is your name? ")
+while tickets_remaining >= 1:
 
-number_of_tickets = input("How many tickets would you like, {}? ".format(name))
-number_of_tickets = int(number_of_tickets)
-price = (number_of_tickets * TICKET_PRICE)
-print("That will cost ${}".format(price))
+    print("Tickets Remaining: {}".format(tickets_remaining))
+    name = input("What is your name? ")
+    number_of_tickets = input("How many tickets would you like, {}? ".format(name))
+    try:
+        number_of_tickets = int(number_of_tickets)
+        
+    except ValueError as err:
+        print("Oh no! That's not a valid value. Try again...")
+    else:
+        price = (number_of_tickets * TICKET_PRICE)
+        print("That will cost ${}".format(price))
+        should_proceed = input("Would you like to proceed? Y/N? ")
+        if should_proceed.lower() == "y":
+            print("SOLD!")
+            tickets_remaining -= number_of_tickets
+            #TODO: Gather credit card information and process it.
+        else:
+            print("Thank you, {}".format(name))
+
+print("Tickets are sold out.")
